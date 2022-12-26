99d66e5ae6669bfd60f8b05695ec9922f10171f8
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-26 15:20:53 +0530

    Update pdf_previewer.py

--- pdf_previewer.py
+++ pdf_previewer.py
@@ -1,8 +1,7 @@
 import streamlit as st
-import PyPDF2 as pdf
+import PyPDF2
 st.title("PDF upload and previewer")
 st.subheader("let's start")
-
 pdf_file=st.file_uploader("upload your pdf file",type=["pdf"])
 if pdf_file is not None:
  st.write(type(pdf_file))
