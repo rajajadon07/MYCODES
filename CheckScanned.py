d4d7616be61c8bf2188117d1bb9972868ba8767b
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-26 15:50:40 +0530

    Add files via upload

---
+++ CheckScanned.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pandas as pd
+import base64,random
+import time,datetime
+import PyPDF2
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import io,random
+from streamlit_tags import st_tags
+from PIL import Image
+import pafy
+import plotly.express as px
+import docx2txt
+import nltk
+
+
+
+
+def show_pdf(file_path):
+    with open(file_path, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+
+def run():
+    st.title("Scanned or Digital PDF Identifier")
+    img = Image.open('./Logo/logo.jpg')
+    st.image(img)
+
+    
+    if True:
+        pdf_file = st.file_uploader("Choose your file", type=["pdf"])
+        st.text(pdf_file.name)
+        
+        
+        if pdf_file is not None:
+            save_image_path = './Temp/'+pdf_file.name
+            
+            directory = r'C:\Users\mokhan\Anaconda3\Brown Field Project\DigOrScan\Test2.pdf'
+            st.text(directory)
+
+            pdfFileObj = open(pdf_file.name, 'rb')
+            pdfReader = PyPDF2.PdfFileReader(pdfFileObj , strict=False)
+            page_data = pdfReader.getPage(0)
+
+            if '/Font' in page_data['/Resources']:
+                st.success("This is digital PDF with below tags")
+                st.text(page_data['/Resources'].keys())
+            else :
+                st.error("This is a scanned pdf with below tags" )
+                st.text(page_data['/Resources'].keys())    
+                              
+                   
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)            
+                                     
+run()
