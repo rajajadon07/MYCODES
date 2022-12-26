4a8049850cec0421d28b678b9b1ff66ca1ef8c9b
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-26 15:42:56 +0530

    Update pdf_previewer.py

--- pdf_previewer.py
+++ pdf_previewer.py
@@ -1,5 +1,5 @@
 import streamlit as st
-inport pdfplumber
+import pdfplumber
 st.title("PDF upload and previewer")
 st.subheader("let's start")
 pdf_file=st.file_uploader("upload your pdf file",type=["pdf"])
