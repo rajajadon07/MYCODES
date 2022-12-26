72a965fa4ffe0703507c4a049bb97e99a40d68b8
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-26 14:24:24 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.number_input1('Enter a number')
-st.number_input2('Enter second number')
+st.number_input1(Enter a number)
+st.number_input2(Enter second number)
 st.result_add(number_input1,number_input2)
 st.write(result_add)
