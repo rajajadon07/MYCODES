343f97714481218d934dcf7c2f1427e4cf217716
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-26 15:35:13 +0530

    Update pdf_previewer.py

--- pdf_previewer.py
+++ pdf_previewer.py
@@ -6,5 +6,8 @@ st.button("Process")
 if pdf_file is not None:
  pdf_details={"pdf_name":pdf_file.name,"pdf_size":pdf_file.size,"pdf_type":pdf_file.type}
  st.write(pdf_details)
+if pdf_file=="application/pdf":
+ read_pdf=pdf_file.read()
+ 
  
  
