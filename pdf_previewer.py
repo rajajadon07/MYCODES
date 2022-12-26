7bdf2afb894a6c5fd29a3776e28f048fdde33f2d
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-26 15:22:35 +0530

    Update pdf_previewer.py

--- pdf_previewer.py
+++ pdf_previewer.py
@@ -3,5 +3,6 @@ import PyPDF2
 st.title("PDF upload and previewer")
 st.subheader("let's start")
 pdf_file=st.file_uploader("upload your pdf file",type=["pdf"])
+st.button("Process")
 if pdf_file is not None:
  st.write(type(pdf_file))
