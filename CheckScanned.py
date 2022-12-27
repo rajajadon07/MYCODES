574b705879fe3281044c4696f41fdc932b8860b0
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-27 16:02:00 +0530

    Update CheckScanned.py

--- CheckScanned.py
+++ CheckScanned.py
@@ -52,7 +52,7 @@ def run():
                 st.text(page_data['/Resources'].keys())    
                               
                    
-            with open(save_image_path, "./Logo/logo.jpg") as f:
+            with open(save_image_path, "wb") as f:
                 f.write(pdf_file.getbuffer())
             show_pdf(save_image_path)            
                                      
