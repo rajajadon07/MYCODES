04fa2fd692328922f2da5d0d4de9deb9bdaaaa1a
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2022-12-29 10:58:03 +0530

    Add files via upload

---
+++ excel_uploder_app.py
@@ -0,0 +1,32 @@
+import streamlit as st 
+import pandas as pd  
+import plotly.express as px
+import base64  
+from io import StringIO, BytesIO
+
+
+
+
+
+st.set_page_config(page_title='Excel Plotter')
+st.title('Excel Plotter 📈')
+st.subheader('Feed me with your Excel file')
+
+uploaded_file = st.file_uploader('Choose a XLSX file', type='xlsx')
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
+    groupby_column = st.selectbox(
+        'What would you like to analyse?',
+        ('Ship Mode', 'Segment', 'Category', 'Sub-Category'),
+       
+fig=px.bar(
+   df_grouped,
+   x=groupby_column,
+   y='Sales'
+   )
+    
+    st.plotly_chart(fig)
+
+   
