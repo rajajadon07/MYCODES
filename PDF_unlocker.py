1296324a1f27de23434255ef36eda3bee5719ee4
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-03 12:42:54 +0530

    Update PDF_unlocker.py

--- PDF_unlocker.py
+++ PDF_unlocker.py
@@ -9,22 +9,22 @@ def main():
      option_op=st.selectbox(
                              'Working with PDF',
                             ('Unlock PDF','Merge PDF')
-      )                   
+     )                   
 
- if option_op == 'Unlock PDF':
-    st.write("Unlock PDF")
-    uploaded_files = st.file_uploader('Choose a PDF file', accept_multiple_files=True)
-      for uploaded_file_data in uploaded_files:
+     if option_op == 'Unlock PDF':
+       st.write("Unlock PDF")
+       uploaded_files = st.file_uploader('Choose a PDF file', accept_multiple_files=True)
+       for uploaded_file_data in uploaded_files:
           bytes_data=uploaded_file_data.read()
           st.write('filename:',uploaded_file_data.name)
 
 
-          pdf_password=st.text_input("PDF password")
-          unlock=st.button("Unlock PDF")
-          if unlock:
-              pdf=pikepdf.open(uploaded_file_data,password=pdf_passsword)
-              pdf.save(uploaded_file_data.name)
-              st.write("File is Successfully unlocked")
+              pdf_password=st.text_input("PDF password")
+       unlock=st.button("Unlock PDF")
+       if unlock:
+           pdf=pikepdf.open(uploaded_file_data,password=pdf_passsword)
+           pdf.save(uploaded_file_data.name)
+           st.write("File is Successfully unlocked")
 
 
        
@@ -37,8 +37,8 @@ def main():
 
 
 
-      if option_op == 'Merge PDF':
-         st.write("Merge PDF")
+  if option_op == 'Merge PDF':
+     st.write("Merge PDF")
 
 
 
@@ -51,6 +51,6 @@ def main():
 
 
 
-      if __name__=='__main__' :
+ if __name__=='__main__' :
            main()
 
