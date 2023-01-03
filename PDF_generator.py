c7feb6e2e71b15dc1ec30b69ed6940f6b89f8b1d
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-03 14:53:40 +0530

    Update PDF_generator.py

--- PDF_generator.py
+++ PDF_generator.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from fpdf import FPDF
 import base64
+import streamlit as st
+import streamlit.components.v1 as components
+
 
 report_text = st.text_area("Report Text")
+components.html(""" <div style="text-align: center"> your-text-here </div> """ )
 
 
 export_as_pdf = st.button("Export Report")
@@ -18,6 +22,6 @@ if export_as_pdf:
     pdf.cell(40, 10, report_text)
     
     html = create_download_link(pdf.output(dest="S").encode("latin-1"), "test")
-    html1=<div style="text-align: center"> your-text-here </div>
+   
 
     st.markdown(html,,html1, unsafe_allow_html=True)
