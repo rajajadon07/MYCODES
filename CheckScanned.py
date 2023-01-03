5b291e34bebe6109fc7e2e368363356dae775895
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-03 16:42:33 +0530

    Update CheckScanned.py

--- CheckScanned.py
+++ CheckScanned.py
@@ -14,7 +14,7 @@ import nltk
 
 
 
-def show_pdf(pdf_file)
+def show_pdf(pdf_file):
     with open(pdf_file, 'rb') as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
