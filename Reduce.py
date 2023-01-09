f1e6aafa80245b1cf6a027ad41b8ad394bd56ba6
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-09 19:24:29 +0530

    Add files via upload

---
+++ Reduce.py
@@ -0,0 +1,12 @@
+from PyPDF2 import PdfReader, PdfWriter
+
+reader = PdfReader("big-old-file.pdf")
+writer = PdfWriter()
+
+for page in reader.pages:
+    writer.add_page(page)
+
+writer.add_metadata(reader.metadata)
+
+with open("smaller-new-file.pdf", "wb") as fp:
+    writer.write(fp)
