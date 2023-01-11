e313046332247a7a6eba3305a600ce0a13ea0da1
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-11 12:20:04 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -2,5 +2,5 @@ from googlesearch import search
 inp=input()
 query = 'inp'
 
-for i search(query,lang='en',start=0,num=20,end=20,pause=2):
+for i in search(query,lang='en',start=0,num=20,end=20,pause=2):
      print(i)
