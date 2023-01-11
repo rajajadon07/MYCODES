52e093b8f77efb14c8003fb017c305955c49b893
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-11 16:14:35 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -12,5 +12,5 @@ elif option==2:
     for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
          st.write(i)
 elif option==3:
-    for i in search(query, tld='co.in'lang='en',num=3,stop=3,pause=2):
+    for i in search(query, tld='co.in',lang='en',num=3,stop=3,pause=2):
       st.write(i)
