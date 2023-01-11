22fc903821a3840c14b338ca11d37d0aba03012b
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-11 12:47:41 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from googlesearch import search
-inp=st.text_input("Enter your text here")
-query = 'inp'
 
+search=st.text_input("Enter your text here")
+query = "search"
 for i in search(query,tld='co.in',lang='en',num=10,stop=10,pause=2):
      st.write(i)
