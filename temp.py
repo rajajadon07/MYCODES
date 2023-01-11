736948af831671b65be36825260e210187bd2130
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-11 16:32:08 +0530

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,20 @@
+
+import streamlit as st
+from googlesearch import search
+import urllib
+from bs4 import BeautifulSoup
+st.code(
+def google_scrape(url):
+    thepage = urllib.urlopen(url)
+    soup = BeautifulSoup(thepage, "html.parser")
+    return soup.title.text
+
+i = 1
+query = 'search this'
+for url in search(query, stop=10):
+    a = google_scrape(url)
+    print str(i) + ". " + a
+    print url
+    print " "
+    i += 1
+)
