a46cbe5e82131eb0268b99ab4258d3d790333739
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-11 16:35:25 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,14 +4,14 @@ from googlesearch import search
 import urllib
 from bs4 import BeautifulSoup
 st.code(
-def google_scrape(url):
+ def google_scrape(url):
     thepage = urllib.urlopen(url)
     soup = BeautifulSoup(thepage, "html.parser")
     return soup.title.text
 
-i = 1
-query = 'search this'
-for url in search(query, stop=10):
+ i = 1
+ query = 'search this'
+ for url in search(query, stop=10):
     a = google_scrape(url)
     print str(i) + ". " + a
     print url
