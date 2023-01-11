ebe275f77ee033ba5cfb8b669795e8bd72516d49
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-11 15:59:09 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -5,5 +5,5 @@ find=st.text_input("Enter your text here")
 query = find
 option=st.selectbox('Select days your want search result for',
                          ('1','2','3'))
-for i in search(query,tld='co.in',lang='en',num,stop=10,pause=2):
+for i in search(query,tld='co.in',lang='en',num=5,stop=10,pause=2):
      st.write(i,option)
