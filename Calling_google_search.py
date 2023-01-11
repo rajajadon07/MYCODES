03ec8fb5b4ebb30d7e73c37f1a4a1205533a5149
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-11 15:58:47 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -3,12 +3,7 @@ from googlesearch import search
 
 find=st.text_input("Enter your text here")
 query = find
-num=st.selectbox('Select days your want search result for',
+option=st.selectbox('Select days your want search result for',
                          ('1','2','3'))
 for i in search(query,tld='co.in',lang='en',num,stop=10,pause=2):
-     if i>1 and i>2:
-      st.write(i)
-     elif i>1 and i<3:
-      st.write(i)
-     else i<2 and i<3:
-      st.write(i)
+     st.write(i,option)
