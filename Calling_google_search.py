3c36bdf116c3d6492fd21e13aa1a00f8ae2ae5b8
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 04:26:49 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -13,6 +13,7 @@ option=st.number_input('Top results', format='%i')
 
 if option <2 and option < 3:
     for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
+     st.write(i)
        
 elif option > 1 and option < 3:
     for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
