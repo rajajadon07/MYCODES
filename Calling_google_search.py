4e27f54aa428fa06557dfcf162dd3a75cde705b7
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 03:33:13 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -7,10 +7,10 @@ option=st.number_input('Insert the number of days')
 if option <2 and option < 3:
  for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
         st.write(i)
-   elif option > 1 and option < 3:
+elif option > 1 and option < 3:
  for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
         st.write(i)
-if option > 1 and option > 2:
+elif option > 1 and option > 2:
  for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
         st.write(i)
 
