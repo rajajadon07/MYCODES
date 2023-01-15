04cef8e705356639d46ff54c34e716ec93419e2b
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 04:20:53 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -4,13 +4,15 @@ from googlesearch import search
 find=st.text_input("Enter your text here")
 query = find
 for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=3):
-   st.write(i)
+   top_results=st.number_input('Top results , format='%i')
+   if i>=top_results:
+      st.write(i)
    
    
    
    
 option=st.number_input('Top results', format='%i')
-
+st.container(
 if option <2 and option < 3:
     for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
         st.write(i)
@@ -20,6 +22,7 @@ elif option > 1 and option < 3:
 elif option > 1 and option > 2:
     for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
         st.write(i)
+)
         
 
 
