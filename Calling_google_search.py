f9a03099329a7addfaaf7aa248805e0e4d60ea7b
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 03:18:54 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -2,11 +2,11 @@ import streamlit as st
 from googlesearch import search
 
 find=st.text_input("Enter your text here")
-query = option
+query = find
 option=st.selectbox('Select days your want search result for',
                          ('1','2','3'))
 
-   for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
+for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
         st.write(i)
 
   
