88fad6a1739de2a60b02f62d8c1e8b65b2b242ea
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 04:10:37 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -3,7 +3,7 @@ from googlesearch import search
 
 find=st.text_input("Enter your text here")
 query = find
-for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=2):
+for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=3):
    st.write(i)
    
    
