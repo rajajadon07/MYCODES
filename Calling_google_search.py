b9689d9d0d5e0a0fb8c4c1c03cbb373be0da51fe
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 04:25:58 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -10,6 +10,7 @@ for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=4):
    
    
 option=st.number_input('Top results', format='%i')
+
 if option <2 and option < 3:
     for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
        
