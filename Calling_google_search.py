895aa9f324fdf1196f6610280a682dc6a1cae902
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 17:16:55 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -4,8 +4,7 @@ from bs4 import BeautifulSoup
 
 import requests
 
-find=st.text_input("Enter your text here")
-query=find
+query=st.text_input("Enter your text here")
 
 for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=2):
      req=requests.get(i)
@@ -20,13 +19,13 @@ for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=2):
 option=st.number_input('Top results', format='%i')
 col1,col2,col3=st.columns(3)
 
- if option <2 and option < 3:
+if option <2 and option < 3:
    for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
      result1=st.write(i)
      col1.header("Top result 1")
 
 
- elif option > 1 and option < 3:
+elif option > 1 and option < 3:
      temp=3
      count=2
      for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
@@ -34,7 +33,7 @@ col1,col2,col3=st.columns(3)
        if count<temp:
           col2.header("Top result 2")
           count=count+1
- elif option > 1 and option > 2:
+elif option > 1 and option > 2:
      temp=4
      count=3
      for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
