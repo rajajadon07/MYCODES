d9a3f67bb68cd4ba3f723a7f612adc0f891ff4b9
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 18:02:36 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -22,10 +22,10 @@ col1,col2,col3=st.columns(3)
 
 if option <2 and option < 3:
       for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
-      result1=st.write(i)
-      col1.header("Top result 1")
-      for title in soup.find_all('title'):
-          st.write(title.get_text())
+         result1=st.write(i)
+         col1.header("Top result 1")
+         for title in soup.find_all('title'):
+            st.write(title.get_text())
 
 
 elif option > 1 and option < 3:
@@ -33,21 +33,21 @@ elif option > 1 and option < 3:
       temp=3
       count=2
       result2=st.write(i)
-      if count<temp:
+         if count<temp:
          col2.header("Top result 2")
-         for title in soup.find_all('title'):
-            st.write(title.get_text())
-            count=count+1
+           for title in soup.find_all('title'):
+              st.write(title.get_text())
+              count=count+1
 elif option > 1 and option > 2:
      for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
      temp=4
      count=3
      result3=st.write(i)
-     if count<temp:
+        if count<temp:
         col3.header("Top result 3")
-        for title in soup.find_all('title'):
-           st.write(title.get_text())
-           count=count+1
+           for title in soup.find_all('title'):
+              st.write(title.get_text())
+              count=count+1
         
 
 
