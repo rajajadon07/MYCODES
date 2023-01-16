9d3926bdf06558cc2b943b085c1a2274cc9b7606
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 12:30:45 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -11,14 +11,15 @@ for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=2):
    
 
 option=st.number_input('Top results', format='%i')
-
+col1,col2,col3=st.columns(3)
 if option <2 and option < 3:
     for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
-     st.write(i)
+     result1=st.write(i)
+     col1.result1("result)
        
 elif option > 1 and option < 3:
     for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
-        st.write(i)
+        col2=st.write(i)
 elif option > 1 and option > 2:
     for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
         st.write(i)
