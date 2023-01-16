cbb15ff47293e2799de52cef760d11ea4ba8e293
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 18:04:22 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -29,7 +29,7 @@ if option <2 and option < 3:
 
 
 elif option > 1 and option < 3:
-      for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
+      for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
          temp=3
          count=2
          result2=st.write(i)
@@ -39,7 +39,7 @@ elif option > 1 and option < 3:
               st.write(title.get_text())
               count=count+1
 elif option > 1 and option > 2:
-     for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
+     for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
         temp=4
         count=3
         result3=st.write(i)
