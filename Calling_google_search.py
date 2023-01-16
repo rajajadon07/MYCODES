8f0f7a48459dd228e2f8a85bfd13762cacf00800
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 14:46:15 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -18,9 +18,9 @@ if option <2 and option < 3:
      result1=st.write(i)
      col1.header("Top result 1")
     
+temp=2
+count=3
 with st.container():      
- temp=3
- count=2
  elif option > 1 and option < 3:
     for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
       result2=st.write(i)
