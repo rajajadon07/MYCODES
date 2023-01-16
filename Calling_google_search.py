99a66625778e7078033bbfbd0a114752239d0348
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 15:03:13 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -28,9 +28,13 @@ elif option > 1 and option < 3:
           col2.header("Top result 2")
           count=count+1
 elif option > 1 and option > 2:
+       temp=4
+       count=3
        for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
          result3=st.write(i)
+         if count<temp
          col3.header("Top result 3")
+         count=count+1
         
 
 
