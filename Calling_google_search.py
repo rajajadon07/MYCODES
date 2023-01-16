6af1a987121fbabe2eb7a4d0e771906bc92acf24
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 17:32:50 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -20,7 +20,6 @@ option=st.number_input('Top results', format='%i')
 col1,col2,col3=st.columns(3)
 
 if option <2 and option < 3:
-    for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
       result1=st.write(i)
       col1.header("Top result 1")
 
