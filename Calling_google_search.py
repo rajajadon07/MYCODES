e1e2daf62f4f5300de32ec3705610ab958536d25
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-16 17:36:17 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -8,11 +8,12 @@ query=st.text_input("Enter your text here")
 
 for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=2):
      req=requests.get(i)
+     st.write(i)
      soup = BeautifulSoup(req.text, 'html.parser')
      st.write("Title of this website: " )
      for title in soup.find_all('title'):
           st.write(title.get_text())
-          st.write(i)
+         
    
    
 
