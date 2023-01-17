c099f9fb4fc6e07c0d218d5c1dec4f49dac3497d
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 11:56:01 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -46,10 +46,9 @@ elif option > 1 and option > 2:
         result3=st.write(i)
         if count<temp:
            col3.header("Top result 3")
-           count=count+1
         for title in soup.find_all('title'):
               st.write(title.get_text())
-              
+              count=count+1
         
 
 
