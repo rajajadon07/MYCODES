fef7781b394601894d7f05678cc1ac366b3b6105
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 12:34:14 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,11 +1,11 @@
 import streamlit as st
-with st.form(key='my_form'):
-       temp=15
-       while temp>0:
-         Question_enter=st.write('question')
-         text_input = st.text_area()
-         temp=temp-1
+form=st.form(key='form')
+temp=15
+while temp>0
+  Question_enter=form.write('question')
+  text_input = form.text_area()
+  temp=temp-1
        
         
          
-       submit_button = st.form_submit_button(label='Submit')
+submit_button = st.form_submit_button(label='Submit')
