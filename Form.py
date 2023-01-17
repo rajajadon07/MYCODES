db44acb1d15906d7c4614c5e60d3c0900abeb08d
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 14:41:09 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,8 +1,9 @@
 import streamlit as st
 with st.form(key='my_form', clear_on_submit=True):
+  title_SOP=st.title('SOP Questions form')
   temp=15
   while temp>0:
-    Question_enter=st.write('question1')
+    Question_enter=st.write('question')
     text_input = st.text_input(label='Enter some text',key=temp)
     temp=temp-1
     
