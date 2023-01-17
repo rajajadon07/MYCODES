d88442d8e8ca079bea143aa9f2dca02249240f16
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 18:01:19 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -50,9 +50,9 @@ elif option > 1 and option < 3:
          result2=st.write(i)
       if count<temp:
            col2.header("Top result 2")
-           count=count+1
-      for title in soup.find_all('title',limit=1):
-              st.write(title.get_text())
+           for title in soup.find_all('title',limit=1):
+              st.write(title.get_text
+                 count=count+1
               
 elif option > 1 and option > 2:
      for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
@@ -61,10 +61,10 @@ elif option > 1 and option > 2:
         result3=st.write(i)
         if count<temp:
            col3.header("Top result 3")
-        for title in soup.find_all('title',limit=1):
+           for title in soup.find_all('title',limit=1):
               st.write(title.get_text())
-              count=count+1
-        
+                  count=count+1
+             
 
 
   
