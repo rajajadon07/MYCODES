3b13461e61a36d9f2e9a8d2c5f753051a9e1e0b0
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 12:03:54 +0530

    Add files via upload

---
+++ Form.py
@@ -0,0 +1,20 @@
+with st.form(key='my_form'):
+        Question_enter=st.write('question1')
+	text_input = st.text_input(label='Enter some text')
+
+        Question_enter=st.write('question1')
+	text_input = st.text_input(label='Enter some text')
+         
+    
+        Question_enter=st.write('question1')
+	text_input = st.text_input(label='Enter some text')
+ 
+ 
+        Question_enter=st.write('question1')
+	text_input = st.text_input(label='Enter some text')
+
+
+        Question_enter=st.write('question1')
+	text_input = st.text_input(label='Enter some text')
+         
+	submit_button = st.form_submit_button(label='Submit')
