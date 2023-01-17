102c8dc0d64830114118835000684d1ffc4d6b6c
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 14:37:42 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,20 +1,8 @@
 with st.form(key='my_form'):
-        Question_enter=st.write('question1')
-	text_input = st.text_input(label='Enter some text')
-
-        Question_enter=st.write('question1')
-	text_input = st.text_input(label='Enter some text')
-         
+  temp=15
+  while temp>0
+    Question_enter=st.write('question1')
+    text_input = st.text_input(label='Enter some text',key=temp)
+    temp=temp-1
     
-        Question_enter=st.write('question1')
-	text_input = st.text_input(label='Enter some text')
- 
- 
-        Question_enter=st.write('question1')
-	text_input = st.text_input(label='Enter some text')
-
-
-        Question_enter=st.write('question1')
-	text_input = st.text_input(label='Enter some text')
-         
-	submit_button = st.form_submit_button(label='Submit')
+  submit_button = st.form_submit_button(label='Submit')
