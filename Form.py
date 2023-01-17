28486c2f048fdf38605f8bfc0c12cd326af23069
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-18 02:59:15 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -5,11 +5,7 @@ with st.form(key='my_form', clear_on_submit=True):
     for arg in argv:
       st.write(arg)
      myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
-                                 
-  
-    st.write("It is the file received from the payroll department, which describes the allowances that had been incurred by the employee/Expat during the secondment period. Below mentioned screenshot is for your reference")
-  else:
-    st.text_input("These are the invoices which are booked by the AP for payment and data has been use for calculation of the amount to be charged")
+                                
 
     
     
