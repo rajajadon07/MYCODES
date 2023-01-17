82e0fd7e6a86839c4ae7eb0e831dcd9dda43ff3b
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 18:35:41 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,10 +1,9 @@
 import streamlit as st
 with st.form(key='my_form', clear_on_submit=True):
   title_SOP=st.title('SOP Questions form')
-  temp=15
-  while temp>0:
-    Question_enter=st.write('question')
-    text_input = st.text_area(label='please enter your answer',key=temp)
-    temp=temp-1
+  
+  Question_enter=st.write('What is Living allowance ?')
+  text_input = st.text_input(label='please enter your answer',key=temp)
+   
     
   submit_button = st.form_submit_button(label='Submit')
