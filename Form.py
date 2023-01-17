0d9dba7fa227e1c12acefc69095671f896ec339f
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 19:30:14 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,10 +1,11 @@
 import streamlit as st
 with st.form(key='my_form', clear_on_submit=True):
   title_SOP=st.title('SOP Questions form')
-  Question_dropdown=st.selectbox(
-                                  'Enter your question',
-                                ('What is Living allowance ?','What is Supplier invoice ?'))
-  if Question_dropdown=="What is Living allowance ?":
+  def myFun(*argv):
+    for arg in argv:
+      
+                                 
+  
     st.write("It is the file received from the payroll department, which describes the allowances that had been incurred by the employee/Expat during the secondment period. Below mentioned screenshot is for your reference")
   else:
     st.text_input("These are the invoices which are booked by the AP for payment and data has been use for calculation of the amount to be charged")
