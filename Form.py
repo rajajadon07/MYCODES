b2d777aa048afe4817fd7f60718023bb99028a54
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 12:25:15 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -3,7 +3,7 @@ with st.form(key='my_form'):
        temp=15
        while temp>0:
          Question_enter=st.write('question')
-         text_input = st.text_area("enter your answer")
+         text_input = st.text_area()
          temp=temp-1
        
         
