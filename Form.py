7a0090013b5bbc14e0fc99aaa6596828040a6195
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 19:11:24 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -6,7 +6,7 @@ with st.form(key='my_form', clear_on_submit=True):
                                 ('What is Living allowance ?','What is Supplier invoice ?'))
   if Question_dropdown=="What is Living allowance ?":
     st.write("It is the file received from the payroll department, which describes the allowances that had been incurred by the employee/Expat during the secondment period. Below mentioned screenshot is for your reference")
-  else
+  else:
     st.text_input(label="Answer","These are the invoices which are booked by the AP for payment and data has been use for calculation of the amount to be charged")
 
   submit_button = st.form_submit_button(label='Submit')
