5716dfc9edfca7459daf1e42d264b5ceaf2e264e
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 13:11:08 +0530

    Update Form1.py

--- Form1.py
+++ Form1.py
@@ -1,13 +1,13 @@
 import streamlit as st
-with st.form(key='Form' , clear_on_submit=True):
- temp=15
- while temp>0:
-   Question_enter=st.write('question')
-   text_input = st.selectbox(label='some text',key=temp)
-   temp=temp-1
+ with st.form(key='form' , clear_on_submit=True):
+   temp=15
+   while temp>0:
+     Question_enter=st.write('question')
+     text_input = st.selectbox(label='some text',key=temp)
+     temp=temp-1
        
         
          
- submit_button = st.form_submit_button(label='Submit')
+   submit_button = st.form_submit_button(label='Submit')
 
  
