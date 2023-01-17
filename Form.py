5a26ad8c3f8b271a8cbfd3014eef60aad3ba7871
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 14:42:03 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -4,7 +4,7 @@ with st.form(key='my_form', clear_on_submit=True):
   temp=15
   while temp>0:
     Question_enter=st.write('question')
-    text_input = st.text_input(label='Enter some text',key=temp)
+    text_input = st.text_area(label='please enter your answer',key=temp)
     temp=temp-1
     
   submit_button = st.form_submit_button(label='Submit')
