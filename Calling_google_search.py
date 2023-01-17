22965a440d11f3d9a2060da9dbcba0a5e92fe1fc
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 15:54:48 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -11,7 +11,7 @@ choice=st.sidebar.selectbox("Country Selection",menu)
 if choice=="USA":
     result="USA"
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2,tbs="qdr:m"):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -22,7 +22,7 @@ if choice=="USA":
 elif choice=="CHINA":
     result="CHINA"
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2,tbs="qdr:m"):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -33,7 +33,7 @@ elif choice=="CHINA":
 elif choice=="RUSSIA":
     result="RUSSIA"
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2,tbs="qdr:m"):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -44,7 +44,7 @@ elif choice=="RUSSIA":
 elif choice=="BRAZIL":
     result="BRAZIL"
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2,tbs="qdr:m"):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -55,7 +55,7 @@ elif choice=="BRAZIL":
 elif choice=="FRANCE":
     result="FRANCE"
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2,tbs="qdr:m"):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -66,7 +66,7 @@ elif choice=="FRANCE":
 elif choice=="GERMANY":
     result="GERMANY"
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2,tbs="qdr:m"):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -77,7 +77,7 @@ elif choice=="GERMANY":
 elif choice=="JAPAN":
     result="JAPAN"
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',country=result ,lang='en',num=5,stop=5,pause=2,tbs="qdr:m"):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -91,7 +91,7 @@ option=st.number_input('Top results', format='%i')
 col1,col2,col3=st.columns(3)
 
 if option <2 and option < 3:
-      for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
+      for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2,tbs="qdr:m"):
          result1=st.write(i)
          col1.header("Top result 1")
          for title in soup.find_all('title'):
@@ -99,7 +99,7 @@ if option <2 and option < 3:
 
 
 elif option > 1 and option < 3:
-      for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2):
+      for i in search(query,tld='co.in',lang='en',num=2,stop=2,pause=2,tbs="qdr:m"):
          temp=3
          count=2
          result2=st.write(i)
@@ -110,7 +110,7 @@ elif option > 1 and option < 3:
               st.write(title.get_text())
               
 elif option > 1 and option > 2:
-     for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2):
+     for i in search(query,tld='co.in',lang='en',num=3,stop=3,pause=2,tbs="qdr:m"):
         temp=4
         count=3
         result3=st.write(i)
