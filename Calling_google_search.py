0a631a9a1a454d6fe59c2b8325d9d48d966f4675
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 17:56:15 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -11,7 +11,7 @@ choice=st.sidebar.selectbox("Country Selection",menu)
 
 if choice=="USA":
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='com',country='choice' ,lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in' ,lang='en',num=5,stop=5,pause=2):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -21,7 +21,7 @@ if choice=="USA":
          
 elif choice=="CHINA":
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='cn',country='CHINA' ,lang='en',num=5,stop=5,pause=2,):
+    for i in search(query,tld='co.in',lang='en',num=5,stop=5,pause=2,):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
@@ -39,7 +39,7 @@ if option <2 and option < 3:
       for i in search(query,tld='co.in',lang='en',num=1,stop=1,pause=2):
          result1=st.write(i)
          col1.header("Top result 1")
-         for title in soup.find_all('title'):
+         for title in soup.find_all('title',limit=1):
             st.write(title.get_text())
 
 
@@ -51,7 +51,7 @@ elif option > 1 and option < 3:
       if count<temp:
            col2.header("Top result 2")
            count=count+1
-      for title in soup.find_all('title'):
+      for title in soup.find_all('title'limit=1):
               st.write(title.get_text())
               
 elif option > 1 and option > 2:
@@ -61,7 +61,7 @@ elif option > 1 and option > 2:
         result3=st.write(i)
         if count<temp:
            col3.header("Top result 3")
-        for title in soup.find_all('title'):
+        for title in soup.find_all('title',limit=1):
               st.write(title.get_text())
               count=count+1
         
