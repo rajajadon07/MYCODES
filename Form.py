048381ad4860e44c401d421aa9e4bd4e7231a3ef
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 18:37:19 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -5,5 +5,4 @@ with st.form(key='my_form', clear_on_submit=True):
   Question_enter=st.write('What is Living allowance ?')
   text_input = st.text_input(label='please enter your answer',key=temp)
    
-    
   submit_button = st.form_submit_button(label='Submit')
