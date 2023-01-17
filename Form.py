d7c26a714ca180975a9d915d533eabdb66d4d5cc
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-17 18:43:05 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,6 +1,6 @@
 import streamlit as st
 with st.form(key='my_form', clear_on_submit=True):
   title_SOP=st.title('SOP Questions form')
-  Question_enter=st.input("text")
+  Question_enter=st.text_input("text")
   text_input = st.text_input(label='please enter your answer')
   submit_button = st.form_submit_button(label='Submit')
