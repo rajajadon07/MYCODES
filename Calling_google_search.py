92059376898aa2dcbe606456312fd8c9df2d3ea4
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-18 14:55:12 +0530

    Update Calling_google_search.py

--- Calling_google_search.py
+++ Calling_google_search.py
@@ -11,7 +11,7 @@ choice=st.sidebar.selectbox("Country Selection",menu)
 
 if choice=="USA":
     query=st.text_input("Please enter your query here")
-    for i in search(query,tld='co.in',hl='en.UK',lang='en',num=5,stop=5,pause=2):
+    for i in search(query,tld='co.in',h1='en.UK',lang='en',num=5,stop=5,pause=2):
           req=requests.get(i)
           soup = BeautifulSoup(req.text, 'html.parser')
           st.write("Title of this website: " )
