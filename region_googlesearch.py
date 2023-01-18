ea43c26cab2b9f19f27d42dafb20d7138621997a
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-18 15:31:56 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -1,13 +1,7 @@
 import streamlit as st
 
-st.set_page_config(page_title="Google Search", page_icon=":mag:", layout="wide")
 
-query = st.text_input("Enter your search query")
-gl = st.selectbox("Select your country region", ("us", "uk", "fr", "de"))
 
-st.write("Results for:", query)
+search_query = st.search_bar(label='Search', region='fr')
 
-results = google_search(query, num_results=10, gl=gl)
-
-for result in results:
-    st.write("- " + result.title)
+if search_query:
