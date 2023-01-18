fdd802b90eb9d310109498b27e427391e9073414
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-18 16:30:21 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -1,9 +1,15 @@
 import streamlit as st
+from serpapi import GoogleSearch
 
+params = {
+    "q" : "Coffee",
+    "location" : "Austin, Texas, United States",
+    "hl" : "en",
+    "gl" : "us",
+    "google_domain" : "google.com",
+    "api_key" : "demo",
+}
 
-
-search_query = st.search_bar(label='Search', region='fr')
-
-if search_query:
-  st.text_input("enter your query")
+query = GoogleSearch(params)
+dictionary_results = query.get_dict()
  
