50de9f40d2d3a69f241cd168f0339f9197539990
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 16:31:59 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -17,7 +17,6 @@ for i in search(query,tld='com',lang='en',num=5,stop=5,pause=2):
           
        data = {'link':[i],'Title':[heading],'source':[sources]}
        df = pd.DataFrame(data)
-       st.dataframe(df)
        for data in df:
             st.write(data)
 
