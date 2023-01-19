332d264aaec95309ef45506ad0fb7404418fd9d2
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 23:08:35 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -3,9 +3,10 @@ import streamlit as st
 import PyPDF2
 import json
 
-pdf=st.file_uploader("upload your file",type=["pdf"])
+file=st.file_uploader("upload your file",type=["pdf"])
+
 # Open the PDF file
-with open('SOP-GBS-0053-TEN-FIN-AR-JEE-Accruals booking-TEN.pdf', 'rb') as pdf_file:
+with open('file.pdf', 'rb') as pdf_file:
     pdf_reader = PyPDF2.PdfFileReader(pdf_file)
     text = ''
     # Loop through all pages
