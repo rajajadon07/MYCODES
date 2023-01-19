b6cd4c66e36cd1016e5ec09d09a61a4c1c30a51d
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 17:03:44 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -5,17 +5,24 @@ import requests
 from bs4 import BeautifulSoup
 
 st.title("Critical information nearby me")
-query=st.text_input("Please enter your query here")
-st.write("Results for:", query)
-for i in search(query,tld='com',lang='en',num=5,stop=5,pause=2):
-      req=requests.get(i)
-      soup = BeautifulSoup(req.text, 'html.parser')
-      for title in soup.find_all('title',limit=1):
-       heading=st.write(title.get_text())
+query = st.text_input("Enter your query")
+if st.button("Search"):
+        def search(query):
+        query = query.replace(" ", "+")
+        URL = f"https://www.google.com/search?q={query}"
+        r = requests.get(url = URL)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        return soup
+    soup = search(query)
+    titles = soup.findAll("div", {"class": "BNeawe iBp4i AP7Wnd"})
+    heading=st.write(titles)
+
+
+
        sources='cnn','bbc','fox','google','cbs'
        no='1','2','3','4','5'
           
-       data = {'s.no':[no],'link':[i],'Title':[heading],'source':[sources]}
+       data = {'s.no':[no],'link':[url],'Title':[heading],'source':[sources]}
        df = pd.DataFrame(data)
        st.dataframe(df)
     
