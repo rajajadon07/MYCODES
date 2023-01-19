c1d3dcfed6d8f12a9a4d228e2e818fdb435a8ebb
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 14:34:26 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -1,15 +1,22 @@
 import streamlit as st
-from serpapi import GoogleSearch
+import pandas as pd
+from googlesearch import search
+import requests
+from bs4 import BeautifulSoup
 
-params = {
-    "q" : "Coffee",
-    "location" : "Austin, Texas, United States",
-    "hl" : "en",
-    "gl" : "us",
-    "google_domain" : "google.com",
-    "api_key" : "demo",
-}
+st.title("Critical information nearby me")
+ query=st.text_input("Please enter your query here")
+    st.write("Results for:", query)
+    for i in search(query,tld='com',lang='en',num=5,stop=5,pause=2):
+         req=requests.get(i)
+          soup = BeautifulSoup(req.text, 'html.parser')
+           for title in soup.find_all('title',limit=1):
+            heading=st.write(title.get_text())
+          
+            data = {'link':[i],'Title':[heading],'source'['cnn','bbc']}
+            df = pd.DataFrame(data, index=[{'S.no'[1,2,3,4,5]}])
+            st.dataframe(df)
+            st.write(df)
 
-query = GoogleSearch(params)
-dictionary_results = query.get_dict()
+
  
