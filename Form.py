90dcbdf655172cea13fd4186628d508be9fe29be
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 23:02:05 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -1,37 +1,18 @@
-import streamlit as st
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import StringIO
+import PyPDF2
 import json
 
+file.pdf=st.file_uploader("upload your file",type[pdf])
+# Open the PDF file
+with open('file.pdf', 'rb') as pdf_file:
+    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+    text = ''
+    # Loop through all pages
+    for page in range(pdf_reader.numPages):
+        text += pdf_reader.getPage(page).extractText()
 
-with st.form(key='my_form', clear_on_submit=True):
-  title_SOP=st.title('SOP Questions form')
-pdf_file=st.file_uploader("upload your pdf file",type=["pdf"])
-st.button("Process")
+# Convert the text to JSON format
+json_data = json.loads(text)
 
-def pdf_to_json(pdf_file):
-    rsrcmgr = PDFResourceManager()
-    retstr = StringIO()
-    codec = 'utf-8'
-    laparams = LAParams()
-    device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-    fp = open(pdf_path, 'rb')
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    password = ""
-    maxpages = 0
-    caching = True
-    pagenos=set()
+# Access the data in the JSON format
+print(json_data['key'])
 
-    for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password,caching=caching, check_extractable=True):
-        interpreter.process_page(page)
-
-    text = retstr.getvalue()
-    fp.close()
-    device.close()
-    retstr.close()
-    return json.loads(text)
-
- 
