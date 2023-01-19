a4a8e6c2df281da0ad4f3d70ae3649b74f28a8c7
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 15:23:00 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -3,15 +3,15 @@ with st.form(key='my_form', clear_on_submit=True):
   title_SOP=st.title('SOP Questions form')
   def myFun(*argv):
     for arg in argv:
-      st.write(arg)
-      input_question=st.text_input(label='enter your question here')
-  myFun('Living allowance ?', 'Supplier invoice ?')
-      if arg='Living allowance':
-        input_question='What is Living allowance ?'
-        answer=st.text_input('It is the file received from the payroll department, which describes the allowances that had been incurred by the employee/Expat during the secondment period. Below mentioned screenshot is for your reference')
-      else:
-        input_question='Supplier invoice '
-        answer='These are the invoices which are booked by the AP for payment and data has been used for calculation of the amount to be charged'
+       st.write(arg)
+       input_question=st.text_input(label='enter your question here')
+       myFun('Living allowance ?', 'Supplier invoice ?')
+        if arg='Living allowance':
+          input_question='What is Living allowance ?'
+          answer=st.text_input('It is the file received from the payroll department, which describes the allowances that had been incurred by the employee/Expat during the secondment period. Below mentioned screenshot is for your reference')
+        else:
+          input_question='Supplier invoice '
+          answer='These are the invoices which are booked by the AP for payment and data has been used for calculation of the amount to be charged'
 
     
     
