37941427501274031b1305b491f74191f0cabecd
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 18:04:02 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -8,18 +8,16 @@ import json
 st.title("Critical information nearby me")
 query = st.text_input("Enter your query")
 
-
+api_key=AIzaSyDYCPvmSL8RHxDIkukU2-VRy9FJA9I9bLk
 if st.button("Search"):
-        def search(query):
+        def search(query,api_key):
           query = query.replace(" ", "+")
-          URL = f"https://www.googleapis.com/customsearch/v1?key={AIzaSyDYCPvmSL8RHxDIkukU2-VRy9FJA9I9bLk}&cx={https://cse.google.com/cse.js?cx=f23358939906b4e32}&q={query}"
-          r = requests.get(URL)
-          st.write(URL)
-          soup = BeautifulSoup(r.text, 'html.parser')
-          for title in soup.find_all('title',limit=1):
-            st.write(title.get_text())
-          return soup
-        soup = search(query)
+          URL = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={https://cse.google.com/cse.js?cx=f23358939906b4e32}&q={query}"
+          r = requests.get(url=URL)
+          data = r.json()
+          return data
+          results = search(query, api_key)
+          st.write(results)
           
          
         
