6053d55ddfed26bafa184d2c82969388cef02d1f
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 17:47:27 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -11,7 +11,7 @@ query = st.text_input("Enter your query")
 if st.button("Search"):
         def search(query):
           query = query.replace(" ", "+")
-          URL = f"https://www.googleapis.com/customsearch/v1?key={AIzaSyDYCPvmSL8RHxDIkukU2-VRy9FJA9I9bLk}&cx={"https://cse.google.com/cse.js?cx=f23358939906b4e32"}&q={query}"
+          URL = f"https://www.googleapis.com/customsearch/v1?key={AIzaSyDYCPvmSL8RHxDIkukU2-VRy9FJA9I9bLk}&cx={https://cse.google.com/cse.js?cx=f23358939906b4e32}&q={query}"
           r = requests.get(URL)
           st.write(URL)
           soup = BeautifulSoup(r.text, 'html.parser')
