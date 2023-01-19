8ac645ebd26ba4331e74dea9e59d5776d0e250cf
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 18:09:20 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -8,15 +8,15 @@ import json
 st.title("Critical information nearby me")
 query = st.text_input("Enter your query")
 
-api_key=AIzaSyDYCPvmSL8RHxDIkukU2-VRy9FJA9I9bLk
+
 if st.button("Search"):
-        def search(query,api_key):
+        def search(query):
           query = query.replace(" ", "+")
-          URL = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={https://cse.google.com/cse.js?cx=f23358939906b4e32}&q={query}"
+          URL = f"https://www.googleapis.com/customsearch/v1?key={AIzaSyDEhUWGKuYW8G3JR3CpStnveTqu1gXrBD4}&cx={f23358939906b4e32}&q={query}"
           r = requests.get(url=URL)
           data = r.json()
           return data
-          results = search(query, api_key)
+          results = search(query)
           st.write(results)
           
          
