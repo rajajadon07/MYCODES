653ccbf8fae96ce02346269fe75d25975602cca6
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-19 17:04:26 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -8,10 +8,10 @@ st.title("Critical information nearby me")
 query = st.text_input("Enter your query")
 if st.button("Search"):
         def search(query):
-        query = query.replace(" ", "+")
-        URL = f"https://www.google.com/search?q={query}"
-        r = requests.get(url = URL)
-        soup = BeautifulSoup(r.content, 'html.parser')
+          query = query.replace(" ", "+")
+          URL = f"https://www.google.com/search?q={query}"
+          r = requests.get(url = URL)
+          soup = BeautifulSoup(r.content, 'html.parser')
         return soup
     soup = search(query)
     titles = soup.findAll("div", {"class": "BNeawe iBp4i AP7Wnd"})
