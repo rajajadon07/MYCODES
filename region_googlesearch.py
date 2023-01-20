e12fba01828ad497543aaf260c0f9336449e7a2f
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-20 20:24:27 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -11,23 +11,23 @@ api_key="AIzaSyDEhUWGKuYW8G3JR3CpStnveTqu1gXrBD4"
 cx="f23358939906b4e32"
 
 if st.button("Search"):
-        def search(query,api_key,cx):
+        def search(query,api_key):
           query = query.replace(" ", "+")
-          URL = f"https://newsapi.org/v2/top-headlines?sources=google-news&apiKey={api_key}/&cx={cx}&q={query}"
+          URL = f"https://newsapi.org/v2/top-headlines?sources=google-news&apiKey="+ api_key +cx={cx} +q={query}"
           r = requests.get(url=URL)
           data = r.json()
           return data
-        results = search(query,api_key,cx)
+        results = search(query,api_key)
         
-        for i in range(10):
-          text = results['items'][i]['snippet']
-          link = results['items'][i]['link']
-          title = results['items'][i]['title']
-          df = pd.DataFrame({'Description': [text], 'link': [link],'title':[title]})
-          st.table(df)
+         news_data = get_news()
+         for article in news_data["articles"]:
+            st.title(article["title"])
+            st.write(article["description"])
+          
+         
 
-         
-         
+
+
           
           
      
