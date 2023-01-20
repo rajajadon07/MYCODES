a8b8bbd859850d52075556835504df5b4ae0f9e2
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-20 20:54:14 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -9,11 +9,12 @@ st.title("Critical information nearby me")
 query = st.text_input("Enter your query")
 st.button("Search")
 
-
+country_code=st.side.selectbox("select your country",
+                  ('us','uk'))
 
 def get_news():
     api_key = "73770f3d51ef4ebbb571859c4ac153c1"
-    url = "https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=" + api_key + query
+    url = "https://newsapi.org/v2/query?sources=google-news&country={country_code}&apiKey={api_key}"
     response = requests.get(url)
     news_data = response.json()
 
