73561f28caaf15998c2416bd0294aec7d55a73bd
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-20 13:53:13 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -3,19 +3,10 @@ import streamlit as st
 import PyPDF2
 import json
 
-file=st.file_uploader("upload your file",type=["pdf"])
+file=st.file_uploader("upload your file",type=["json"])
 
 # Open the PDF file
-with open('file.pdf', 'rb') as pdf_file:
-    pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-    text = ''
-    # Loop through all pages
-    for page in range(pdf_reader.numPages):
-        text += pdf_reader.getPage(page).extractText()
-
-# Convert the text to JSON format
-json_data = json.loads(text)
-
-# Access the data in the JSON format
-print(json_data['key'])
+with open('export.json', 'rb') as f:
+ data=json.load(f)
+ st.write(data)
 
