37e2674592fef6cea2468c574d98782df2e86287
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-23 12:40:55 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -8,14 +8,14 @@ from gnewsclient import gnewsclient
 country=st.sidebar.selectbox('enter your country',
                      ('us','uk'))
 
-client = gnewsclient.NewsClient(language='english', location='country', topic='Business', max_results=50)
+client = gnewsclient.NewsClient(language='english', location='country', topic='Business', max_results=10)
 client.language = 'en'
 client.location = 'United States'
 client.topic = 'top_stories'
 
 data = client.get_news()
 
-for i in range(50):
+for i in range(5):
     st.write(data[i]['title'])
     st.write(data[i]['link'])
     
