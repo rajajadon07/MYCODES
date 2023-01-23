e02fd59849ab359893adcc9c2d6422c78504695d
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-23 14:10:41 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -7,12 +7,13 @@ import json
 from gnews import GNews
 
 topic=st.text_input('enter your query')
-keyword=('Threats,Risk,Danger,Riots, issues , pandemic , riots, agression')
-country = st.sidebar.selectbox('select your country',
+if st.button('Search'):
+  keyword=('Threats,Risk,Danger,Riots, issues , pandemic , riots, agression')
+  country = st.sidebar.selectbox('select your country',
                               ('us','uk'))
-stories= GNews.get_news_by_topic(topic) 
+  stories= GNews.get_news_by_topic(topic) 
 
-for story in top_stories:
+  for story in top_stories:
     st.markdown(f"- [{story['title']}]({story['link']})")
     st.markdown(story['description'])
 
