8f0410d6acbd5c01a68300a241752986f5ca1de6
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-23 11:56:13 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -13,21 +13,23 @@ cx="f23358939906b4e32"
 country_code=st.sidebar.selectbox("select your country",
                   ('us','uk'))
 if st.button("Search"):
+  
  def get_news():
-    url = "https://www.googleapis.com/customsearch/v1?key={api_key}&cx={cx}&q={query}"
+    url = "https://www.google.com/search?q={}&tbm=nws".format(query)"
     response = requests.get(url)
     news_data = response.json()
     st.write(news_data)
     return news_data
 
  def main():
+  
     news_data = get_news()
     for article in news_data:
         st.title(article["title"])
         st.write(article["link"])
         st.write(article["url"])
        
-if __name__ == "__main__":
+  if __name__ == "__main__":
       main()
 
     
