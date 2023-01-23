45da0f0921b96f2f826b991cfb16e7fcf0b1c1e9
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-23 16:45:14 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -13,12 +13,10 @@ api_key = '96cff03d866a222cb5837f417a57eb85'
 query=st.text_input('enter your query')        
 topic = st.sidebar.selectbox("Topic", ["health","science","breaking news"])
 country = st.sidebar.selectbox("Country", ["us", "in", "gb", "fr", "jp","gb"])
-date_from = st.date_input("From date:", datetime.now() - timedelta(days=30))
-date_to = st.date_input("To date:", datetime.now())
 
 if st.button('Search'):
            
-   url = f'https://gnews.io/api/v3/search?q={query}&topic={topic}&country={country}&date_from={date_from}&date_to={date_to}&token={api_key}'
+   url = f'https://gnews.io/api/v3/search?q={query}&topic={topic}&sort_by=publishedAt&country={country}&token={api_key}'
    news = requests.get(url).json()
                                 
    for article in news['articles']:
