e6f4c94cdecb8a8fe117f9b81ebbc11823bfc388
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-23 15:07:00 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -8,21 +8,22 @@ from gnews import GNews
 
 
 api_key = '96cff03d866a222cb5837f417a57eb85'
-country_id=st.sidebar.selectbox('select your country',
-                                ('us','uk','br'))
+topic = st.selectbox("Topic", ["health", "sports", "entertainment", "science", "technology"])
+country = st.selectbox("Country", ["us", "in", "gb", "fr", "jp"])
 
 query=st.text_input('enter your query')
 if st.button('Search'):
-   url = f'https://gnews.io/api/v3/search?q={query}&topic=health&sort_by=publishedAt&country={country_id}&token={api_key}'
+   url = f'https://gnews.io/api/v3/search?q={query}&topic={topic}&sort_by=publishedAt&country={country}&token={api_key}'
    news = requests.get(url).json()
                                 
    for article in news['articles']:
       st.write("Title:", article['title'])
       st.write("Published at:", article['publishedAt'])
+      st.write("description":", article['description'])
       st.write("Source:", article['source']['name'])
       st.write("Link:", article['url'])
-      
-
+     
+ 
 
 
 
