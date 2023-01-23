81fa4c1f5e10a3dae7c2cf24bce03d9dd936103c
rajajadon07 <121421632+rajajadon07@users.noreply.github.com>
2023-01-23 16:08:27 +0530

    Update region_googlesearch.py

--- region_googlesearch.py
+++ region_googlesearch.py
@@ -8,12 +8,15 @@ from gnews import GNews
 
 
 api_key = '96cff03d866a222cb5837f417a57eb85'
-topic = st.sidebar.selectbox("Topic", ["travel"])
-country = st.sidebar.selectbox("Country", ["us", "in", "gb", "fr", "jp"])
+topic = st.sidebar.selectbox("Topic", ["health,science,breaking news"])
+country = st.sidebar.selectbox("Country", ["us", "in", "gb", "fr", "jp","gb"])
+date_from = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
+date_to = datetime.now().strftime("%Y-%m-%d")
 
-query=st.text_input('enter your query')
+query=st.text_input('enter your query')        
 if st.button('Search'):
-   url = f'https://gnews.io/api/v3/search?q={query}&topic={topic}&sort_by=publishedAt&country={country}&token={api_key}'
+   url = f'https://gnews.io/api/v3/search?q={query}&topic={topic}&sort_by=publishedAt&country={country}&date_from = (datetime.now() - timedelta(days=7)).strftime("%Y-%m-%d")
+date_to = datetime.now().strftime("%Y-%m-%d")&token={api_key}'
    news = requests.get(url).json()
                                 
    for article in news['articles']:
